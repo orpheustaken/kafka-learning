493917f38b841a9007325c5f03c5d5b88ced5a8b
Matheus Vinicius <matheusvinicius7000@gmail.com>
2023-04-12 01:57:33 -0300

    Kafka: Basics

--- kafka-basics/build.gradle.kts
+++ kafka-basics/build.gradle.kts
@@ -10,6 +10,15 @@ repositories {
 }
 
 dependencies {
+    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+    implementation("org.apache.kafka:kafka-clients:3.4.0")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.7")
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
+    testImplementation("org.slf4j:slf4j-simple:2.0.7")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
