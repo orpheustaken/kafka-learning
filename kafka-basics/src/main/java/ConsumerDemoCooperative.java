d1c963aac574905e3e279d3ca06fb2a389032760
Matheus Vinicius <matheusvinicius7000@gmail.com>
2023-04-16 16:52:54 -0300

    Kafka Consumer: Cooperative Rebalance Strategy

---
+++ kafka-basics/src/main/java/ConsumerDemoCooperative.java
@@ -0,0 +1,77 @@
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.CooperativeStickyAssignor;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoCooperative {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsumerDemoCooperative.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("Kafka ConsumerDemoCooperative Class");
+
+        String groupId = "java-app";
+        String topic = "demo.java.topic";
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        // Changes the default RangeAssignor to the CooperativeStickyAssignor
+        properties.setProperty("partition.assignment.strategy", CooperativeStickyAssignor.class.getName());
+
+        // Strategy for Static Assignment, each consumer has an Id
+//        properties.setProperty("group.instance.id", "...");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        final Thread mainThread = Thread.currentThread();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            public void run() {
+                log.info("Detected a shutdown, will exit by calling consumer.wakeup()...");
+
+                consumer.wakeup();
+
+                try {
+                    mainThread.join();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        try {
+            consumer.subscribe(Arrays.asList(topic));
+
+            while (true) {
+                log.info("Polling data from Kafka Topic " + topic);
+
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(5000));
+
+                for (ConsumerRecord<String, String> record : records) {
+                    log.info("Key: " + record.key() + ", Value: " + record.value());
+                    log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+                }
+            }
+        } catch (WakeupException e) {
+            log.info("Consumer is starting to shutdown");
+        } catch (Exception e) {
+            log.error("Unexpected exception...");
+        } finally {
+            consumer.close();
+            log.info("The consumer is now gracefully handling shutting down");
+        }
+    }
+}
