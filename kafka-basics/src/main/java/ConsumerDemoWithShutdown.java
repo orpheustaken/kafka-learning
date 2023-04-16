5b1a98e91540e1607c09a2511b8ceb11acebdfad
Matheus Vinicius <matheusvinicius7000@gmail.com>
2023-04-16 16:12:06 -0300

    Kafka Consumer: More information on KafkaConsumer.wakeup()

--- kafka-basics/src/main/java/ConsumerDemoWithShutdown.java
+++ kafka-basics/src/main/java/ConsumerDemoWithShutdown.java
@@ -16,6 +16,16 @@ import java.util.Properties;
  * When the virtual machine begins its shutdown sequence it will start all registered shutdown hooks in some unspecified order and let them run concurrently.
  * When all the hooks have finished it will then run all uninvoked finalizers if finalization-on-exit has been enabled.
  * Finally, the virtual machine will halt.
+ * <p>
+ * <p>
+ * KafkaConsumer.wakeup()
+ * Exception used to indicate preemption of a blocking operation by an external thread.
+ * For example, KafkaConsumer.wakeup() can be used to break out of an active KafkaConsumer.poll(long), which would raise an instance of this exception.
+ * <p>
+ * <p>
+ * Basically, if you use consumer.poll(Integer.MAX_VALUE) the consumer will block until a message is fetched. In this case, if you would like to stop consumption you can call consumer.wakeup() (from an other thread) and catch the exception to shutdown properly.
+ * <p>
+ * Also, while commiting your offsets synchronously a call to consumer.wakeup() will throw a WakeupException .
  */
 
 // That is, a shutdown hook keeps the JVM running until the hook has terminated (returned from the run() method.
@@ -29,24 +39,14 @@ public class ConsumerDemoWithShutdown {
         String groupId = "java-app";
         String topic = "demo.java.topic";
 
-        // Create Consumer Properties
-        // Connect to localhost
         Properties properties = new Properties();
         properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
-        // Set Consumer Properties
-        // To deserialize byte messages back to the actual data
         properties.setProperty("key.deserializer", StringDeserializer.class.getName());
         properties.setProperty("value.deserializer", StringDeserializer.class.getName());
 
         properties.setProperty("group.id", groupId);
-
-        // Possible values for this property are:
-        //  none: if don't have existing consumer group, then fail
-        //  earliest: starts reading from the beginning of the topic
-        //  latest: reads only new messages
         properties.setProperty("auto.offset.reset", "earliest");
 
-        // Create a Consumer
         KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
 
         // Get a reference to the main thread
@@ -54,7 +54,6 @@ public class ConsumerDemoWithShutdown {
         final Thread mainThread = Thread.currentThread();
 
         // All this shutdown stuff allows for the program to shutdown correctly, finishing all the computing and connection to Kafka Topic
-        // Kinda like signal in C?
 
         // Adding shutdown hook
         Runtime.getRuntime().addShutdownHook(new Thread() {
@@ -75,29 +74,23 @@ public class ConsumerDemoWithShutdown {
             }
         });
 
+        // Knowing that consumer.poll will throw a WakeupException when shutting down
+        // That's why it's needed to surround this with a catch to handle this expected exception
         try {
-            // Subscribe to a topic
-            // Using Arrays.asList it's possible to subscribe to multiple topics
             consumer.subscribe(Arrays.asList(topic));
 
             // Poll for data
             while (true) {
                 log.info("Polling data from Kafka Topic " + topic);
 
-                // Poll data and have a timeout of 5 seconds if there's nothing
-                // Returns a collection (data structure for group of objects, similar to Arrays) of records
                 ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(5000));
 
-                // Iterates over each record from the collection polled from the topic and logs the key, value, partition and offset
                 for (ConsumerRecord<String, String> record : records) {
                     log.info("Key: " + record.key() + ", Value: " + record.value());
                     log.info("Partition: " + record.partition() + ", Offset: " + record.offset());
                 }
             }
-        }
-        // Knowing that consumer.poll will throw a WakeupException when shutting down
-        // That's why is needed to surround this with a catch to handle this expected exception
-        catch (WakeupException e) {
+        } catch (WakeupException e) {
             log.info("Consumer is starting to shutdown");
         }
         // This catch, otherwise, is to handle a unexpected exception...
