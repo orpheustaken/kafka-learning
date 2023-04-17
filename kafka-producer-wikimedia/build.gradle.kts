6236a1543c45d2ea51d4d1d93658517e7dbcbf5c
Matheus Vinicius <matheusvinicius7000@gmail.com>
2023-04-17 01:43:33 -0300

    Wikimedia Project: Add dependencies and Producer Class

--- kafka-producer-wikimedia/build.gradle.kts
+++ kafka-producer-wikimedia/build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.kafka:kafka-clients:3.4.0")
+    implementation("org.slf4j:slf4j-api:2.0.7")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.launchdarkly:okhttp-eventsource:4.1.0")
 }
 
 tasks.test {
---
+++ kafka-producer-wikimedia/src/main/java/WikimediaChangesProducer.java
@@ -0,0 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WikimediaChangesProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(WikimediaChangesProducer.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("WikimediaChangesProducer Class");
+    }
+}
