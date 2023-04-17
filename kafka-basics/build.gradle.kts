01b34a22c7df75f3a7a61049edd4787e0dab4520
Matheus Vinicius <matheusvinicius7000@gmail.com>
2023-04-17 00:04:41 -0300

    Overall: New side project and cleanup

--- kafka-basics/build.gradle.kts
+++ kafka-basics/build.gradle.kts
@@ -10,19 +10,10 @@ repositories {
 }
 
 dependencies {
-    // https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
     implementation("org.apache.kafka:kafka-clients:3.4.0")
-
-    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
     implementation("org.slf4j:slf4j-api:2.0.7")
-
-    // https://mvnrepository.com/artifact/org.slf4j/slf4j-simple
     implementation("org.slf4j:slf4j-simple:2.0.7")
-
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
-    useJUnitPlatform()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "kafka-learning"
 include("kafka-basics")
+include("kafka-producer-wikimedia")
