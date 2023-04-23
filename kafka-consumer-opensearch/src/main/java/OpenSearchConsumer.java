2e019215b983312cfded8d4c8a01568f9f072f62
Matheus Vinicius <matheusvinicius7000@gmail.com>
2023-04-23 07:30:00 -0300

    Wikimedia Project: Kafka Consumer and OpenSearch data sending

--- kafka-consumer-opensearch/src/main/java/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/OpenSearchConsumer.java
@@ -4,16 +4,26 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
 import org.opensearch.client.RequestOptions;
 import org.opensearch.client.RestClient;
 import org.opensearch.client.RestHighLevelClient;
 import org.opensearch.client.indices.CreateIndexRequest;
 import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class OpenSearchConsumer {
     public static RestHighLevelClient createOpenSearchClient() {
@@ -47,11 +57,29 @@ public class OpenSearchConsumer {
         return restHighLevelClient;
     }
 
+    private static KafkaConsumer<String, String> createKafkaConsumer() {
+        String groupId = "wikimedia-java-app";
+        String bootstrapServer = "127.0.0.1:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", bootstrapServer);
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("group.id", groupId);
+        properties.setProperty("auto.offset.reset", "earliest");
+
+        return new KafkaConsumer<>(properties);
+    }
+
+
     public static void main(String[] args) throws IOException {
         Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
 
         // Create OpenSearch Client
         RestHighLevelClient openSearchClient = createOpenSearchClient();
+
+        // Create Kafka Consumer
+        KafkaConsumer<String, String> consumer = createKafkaConsumer();
 
         // Create index on OpenSearch if it doesn't exist
         try (openSearchClient) {
@@ -66,9 +94,28 @@ public class OpenSearchConsumer {
             }
         }
 
-        // Create Kafka Client
+        // Subscribe Consumer to topic
+        consumer.subscribe(Collections.singleton("wikimedia.recentchange"));
 
-        // Main Code Logic
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(3000));
+
+            int recordCount = records.count();
+            log.info("Received " + recordCount + " records");
+
+            for (ConsumerRecord<String, String> record : records) {
+                log.info(record.value());
+            }
+
+            for (ConsumerRecord<String, String> record : records) {
+                // Send record into OpenSearch
+                IndexRequest indexRequest = new IndexRequest("wikimedia").source(record.value(), XContentType.JSON);
+
+                // Not working for some unholy reason
+//                IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+//                log.info(response.getId());
+            }
+        }
 
         // Closure
     }
