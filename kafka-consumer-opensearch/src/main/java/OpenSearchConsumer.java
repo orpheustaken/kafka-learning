ed4a97f009c6d7c152569070823c0938715c5729
Matheus Vinicius <matheusvinicius7000@gmail.com>
2023-04-23 05:18:52 -0300

    Wikimedia Project: OpenSearch connection stuff

--- kafka-consumer-opensearch/src/main/java/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/OpenSearchConsumer.java
@@ -1,5 +1,49 @@
-public class OpenSearchConsumer {
-    public static void main(String[] args) {
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
 
+import java.net.URI;
+
+public class OpenSearchConsumer {
+    public static RestHighLevelClient createOpenSearchClient() {
+        // Create OpenSearch Client
+        String connString = "http://localhost:9200";
+
+        // Build a URI from connection string
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connString);
+        // Extract login information if it exists
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // Rest client without security
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), "http")));
+        } else {
+            // REST client with security
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                            .setHttpClientConfigCallback(
+                                    httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+
+
+        }
+
+        return restHighLevelClient;
     }
+    // Create Kafka Client
+
+    // Main Code Logic
+
+    // Closure
 }
